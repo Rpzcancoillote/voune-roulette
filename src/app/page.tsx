dfeb00b8d0f5fd0f0ae3fc08460f0977145cf2be
Rpzcancoillote <co.gallay@gmail.com>
2025-09-19 19:16:08 +0200

    [Timer] - Adding timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ const numbers = [
 ];
 
 // Durée du timer avant chaque spin (en secondes)
-const TIMER_DURATION = 30;
+const TIMER_DURATION = 25;
 
 // Nombre de tours complets avant de s'arrêter
 const SPIN_TOURS = 3;
